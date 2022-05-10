cf8bb3a9c23f34055ce88f5b567a70cbee5a6b63
MounikaMokamatam <98936078+MounikaMokamatam@users.noreply.github.com>
2022-05-10 13:06:26 -0400

    Final Changes

--- client/src/main.ts
+++ client/src/main.ts
@@ -2,7 +2,7 @@ import { createApp } from 'vue'
 import { createPinia } from 'pinia';
 import 'bulma';
 import '@fortawesome/fontawesome-free/css/all.css';
-import './assets/styles/main.scss';
+import './assets/style/main.scss';
 
 import App from './App.vue'
 import router from './router';
